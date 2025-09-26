f9e4c4bc50805e38d212c4271b48c22f54e4fcd8
Christopher Crow <0chriscrew0@gmail.com>
2025-09-26 01:07:35 -0500

    add basic REPL functionality

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,24 @@
+import { createInterface } from "node:readline";
+
 export function cleanInput(input: string): string[] {
   return input.toLowerCase().trim().split(" ");
+}
+
+export function startREPL() {
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex > ",
+  });
+
+  rl.prompt();
+  rl.on("line", (input: string) => {
+    const inputWords = cleanInput(input);
+    if (inputWords.length === 0) {
+      rl.prompt();
+    } else {
+      console.log(`Your command was: ${inputWords[0]}`);
+      rl.prompt();
+    }
+  });
 }
