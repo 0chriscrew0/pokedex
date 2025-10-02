8a271f2c59e63f91d2663e00da0f1bb0ee56e705
Christopher Crow <0chriscrew0@gmail.com>
2025-10-01 21:08:47 -0500

    add map and mapb commands

--- src/command_exit.ts
+++ src/command_exit.ts
@@ -1,7 +1,7 @@
-import { State } from "./state.js";
+import type { State } from "./state.js";
 
-export function commandExit(state: State) {
+export async function commandExit(state: State) {
   console.log("Closing the Pokedex... Goodbye!");
-  state.repl.close();
+  state.readline.close();
   process.exit(0);
 }
--- src/repl.ts
+++ src/repl.ts
@@ -1,43 +1,40 @@
 import { State } from "./state.js";
 
+export async function startREPL(state: State) {
+  state.readline.prompt();
+
+  state.readline.on("line", async (input) => {
+    const words = cleanInput(input);
+    if (words.length === 0) {
+      state.readline.prompt();
+      return;
+    }
+
+    const commandName = words[0];
+
+    const cmd = state.commands[commandName];
+    if (!cmd) {
+      console.log(
+        `Unknown command: "${commandName}". Type "help" for a list of commands.`
+      );
+      state.readline.prompt();
+      return;
+    }
+
+    try {
+      await cmd.callback(state);
+    } catch (e) {
+      console.log((e as Error).message);
+    }
+
+    state.readline.prompt();
+  });
+}
+
 export function cleanInput(input: string): string[] {
   return input
     .toLowerCase()
     .trim()
     .split(" ")
     .filter((word) => word !== "");
-}
-
-export function startREPL(state: State) {
-  const rl = state.repl;
-
-  rl.prompt();
-
-  rl.on("line", async (input) => {
-    const words = cleanInput(input);
-    if (words.length === 0) {
-      rl.prompt();
-      return;
-    }
-
-    const commandName = words[0];
-
-    const commands = state.commands;
-    const cmd = commands[commandName];
-    if (!cmd) {
-      console.log(
-        `Unknown command: "${commandName}". Type "help" for a list of commands.`
-      );
-      rl.prompt();
-      return;
-    }
-
-    try {
-      cmd.callback(state);
-    } catch (e) {
-      console.log(e);
-    }
-
-    rl.prompt();
-  });
 }
